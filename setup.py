b5947acd8c70d2a0630d4cdb75e8a32f17d26c0f
Arseni Dudko <arsenidudko@mail.ru>
2018-03-01 17:44:25 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.0.1',
+    description='Database backups locally or to AWS S3.',
+    long_description=readme,
+    author='Arseni Dudko',
+    author_email='arseni_dudko@mail.ru',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[]
+)
